9f3ccab9e402358bb0d7392ae9638fcf96dfe7d6
Matheus Sambiase <matheus.e.sambiase@gmail.com>
2018-03-27 23:43:09 -0300

    adding skeleton of gitApi request

--- app/src/main/java/br/com/concretechallenge/api/GithubApi.kt
+++ app/src/main/java/br/com/concretechallenge/api/GithubApi.kt
@@ -1,12 +1,11 @@
 package br.com.concretechallenge.api
 
+import br.com.concretechallenge.model.RepositoryData
 import retrofit2.http.GET
 
 interface GithubApi {
 
     @GET("/search/repositories")
-    fun searchRepos():List<RepositoriesData> (
-
-    )
+    fun searchRepos():List<RepositoryData>
 
 }
--- app/src/main/java/br/com/concretechallenge/model/RepositoryData.kt
+++ app/src/main/java/br/com/concretechallenge/model/RepositoryData.kt
@@ -4,4 +4,4 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class RepositoryData():Parcelable
+data class RepositoryData(val name:String):Parcelable
